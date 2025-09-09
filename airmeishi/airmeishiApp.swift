e35a155bca479c1822a5a3ab9a7a1694395f1210
kidneyweakx <kidneyweakx@gmail.com>
2025-09-09 22:15:57 +0900

    Initial Commit

---
+++ airmeishi/airmeishiApp.swift
@@ -0,0 +1,17 @@
+//
+//  airmeishiApp.swift
+//  airmeishi
+//
+//  Created by kidneyweak on 2025/09/09.
+//
+
+import SwiftUI
+
+@main
+struct airmeishiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ airmeishiTests/airmeishiTests.swift
@@ -0,0 +1,16 @@
+//
+//  airmeishiTests.swift
+//  airmeishiTests
+//
+//  Created by kidneyweak on 2025/09/09.
+//
+
+import Testing
+
+struct airmeishiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
